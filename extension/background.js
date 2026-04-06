a576d36dee014753b231175015d842e2141d7b93
sakab413 <sakabakram4@gmail.com>
2026-04-06 17:41:48 +0200

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,16 +1,17 @@
-chrome.alarms.create("checkCashback", { periodInMinutes: 1 });
-
-chrome.alarms.onAlarm.addListener(() => {
-  fetch("https://mijn-stripe-app.onrender.com")
-    .then(res => res.json())
+setInterval(() => {
+    // VERVANG DE LINK HIERONDER DOOR JOUW ECHTE RENDER LINK!
+    fetch("https://mijn-stripe-app.onrender.com")
+    .then(response => response.json())
     .then(data => {
-      if (data.deals && data.deals.length > 0) {
-        chrome.notifications.create({
-          type: "basic",
-          iconUrl: "icon.png", // Zorg dat je een klein icon.png in de map hebt
-          title: "Geld Terug! 🎉",
-          message: `Je hebt zojuist €${data.deals[0].cashback} cashback verdiend bij ${data.deals[0].shop}!`
-        });
-      }
-    });
-});
+        if (data.status === "found") {
+            chrome.notifications.create({
+                type: 'basic',
+                iconUrl: 'icon.png',
+                title: 'Geld Terug Gevonden! 🎉',
+                message: 'Je hebt €' + data.deals[0].cashback + ' verdiend bij ' + data.deals[0].shop,
+                priority: 2
+            });
+        }
+    })
+    .catch(error => console.log("Nog geen data gevonden..."));
+}, 60000); // Checkt elke minuut
